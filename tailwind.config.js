e7c0d64de8595736c491003249276fdcc5fe9059
DedsID <dedii.maryadi@gmail.com>
2022-10-23 22:24:34 +0700

    third commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+      'u-darkblue': '#2b3d68',
+      'u-softblue': '#9797a5',
+      },
+    },
+  },
+  plugins: [],
+}
