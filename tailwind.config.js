7bdf31bb0f5fb71534c7a4a996785b7edaf1c684
DedsID <dedii.maryadi@gmail.com>
2022-10-24 22:17:52 +0700

    menambah aos

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
+
       colors: {
       'u-darkblue': '#2b3d68',
       'u-softblue': '#9797a5',
       },
+
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      }
     },
   },
   plugins: [],
