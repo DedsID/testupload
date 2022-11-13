c6b8808e626be6fcc87a3eec17eb6dfcbe9db98a
DedsID <dedii.maryadi@gmail.com>
2022-11-13 22:05:58 +0700

    responsive add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,29 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
+      screens: {
+        'xs': '240px',
+      },
 
       colors: {
-      'u-darkblue': '#2b3d68',
-      'u-softblue': '#9797a5',
+        'u-darkblue': '#2b3d68',
+        'u-softblue': '#9797a5',
       },
 
       animation: {
         fadeIn: "fadeIn 2s ease-in forwards"
       },
-      
+
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 }
         }
-      }
+      },
+
+      variants: {
+        animation: ["motion-safe"]
+      },
     },
   },
   plugins: [],
